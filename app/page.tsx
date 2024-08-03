2f9aa422d9aa62a715b8a10bc44d00ee2d6e58c1
Welf9i <welfkimedamine@gmail.com>
2024-08-03 16:53:43 +0100

    Add svg icon

--- app/page.tsx
+++ app/page.tsx
@@ -1,9 +1,14 @@
-import CodeEditor from './components/CodeEditor'
+import CodeEditor from './components/CodeEditor';
 
 export default function Home() {
   return (
-    <main>
-      <CodeEditor />
+    <main className="min-h-screen flex flex-col">
+      <div className="flex-grow">
+        <CodeEditor />
+      </div>
+      <footer className="bg-gray-800 text-white py-4 w-full flex justify-center items-center">
+        <p className="text-sm text-white/70">&copy; {new Date().getFullYear()} Mohamed Amine Welfeki. All rights reserved.</p>
+      </footer>
     </main>
-  )
+  );
 }
